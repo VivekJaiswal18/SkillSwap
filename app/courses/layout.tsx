0f5a87de5b2c12d9da07e9269a7d827ce83b8cfb
VivekJaiswal18 <96608169+VivekJaiswal18@users.noreply.github.com>
2024-09-01 04:57:05 +0530

    Update Readme

--- app/courses/layout.tsx
+++ app/courses/layout.tsx
@@ -1,85 +1,4 @@
-// import "../globals.css";
-// import type { Metadata } from "next";
-// import { Inter, Poppins } from "next/font/google";
-// import { ClerkProvider } from "@clerk/nextjs";
-// import Providers from "@/components/Provider";
-// import Navbar from "@/components/Navbar";
-// import { Toaster } from "@/components/ui/toaster";
-// import localFont from "next/font/local";
-// import { cn } from "@/lib/utils";
-// // import { EdgeStoreProvider } from "@/lib/edgestore";
-// import { siteConfig } from "@/config/site";
-// import WagmiConfigProvider from "../../components/WagmiConfigProvider"
-// import CourseSideBar from "@/components/UnitSide";
-// import CourseMarketplace from "../courses/page";
-// import Footer from "@/components/Footer";
-// // import '@rainbow-me/rainbowkit/styles.css';
-// // import {
-// //   getDefaultConfig,
-// //   RainbowKitProvider,
-// // } from '@rainbow-me/rainbowkit';
-// // import { WagmiProvider } from 'wagmi';
-// // import {
-// //   mainnet,
-// //   polygon,
-// //   optimism,
-// //   arbitrum,
-// //   base,
-// // } from 'wagmi/chains';
-// // import {
-// //   QueryClientProvider,
-// //   QueryClient,
-// // } from "@tanstack/react-query";
-
-
-// const inter = Inter({ subsets: ["latin"] });
-
-// const poppins = Poppins({ subsets: ["latin-ext"], weight: "400" });
-
-// export const metadata: Metadata = {
-//   title: {
-//     default: "SkillSwap - Learn for the best ",
-//     template: `%s -  ${siteConfig.name}`,
-//   },
-// };
-
-// const fontHeading = localFont({
-//   src:  '../../public/CalSans-SemiBold.ttf',
-//   variable: "--font-heading",
-// });
-
-
-
-
-// export default function RootLayout({
-//   children,
-// }: {
-//   children: React.ReactNode;
-// }) {
-//   return (
-//     <html lang="en">
-//       {/* <WagmiProvider config={config}>
-//       <QueryClientProvider client={queryClient}>
-//         <RainbowKitProvider> */}
-//         <WagmiConfigProvider>
-//         <Providers>
-//             <Navbar />
-//             <body className={cn(poppins.className , fontHeading.variable)}>{children}</body>
-//           <Toaster />
-//           <Footer/>
-//         </Providers>
-//         </WagmiConfigProvider>
-//         {/* </RainbowKitProvider>
-//       </QueryClientProvider>
-//     </WagmiProvider> */}
-//     </html>
-//   );
-// }
-
-
-
-
-"use client" 
+// "use client" 
 import "../globals.css";
 import type { Metadata } from "next";
 import { Inter, Poppins } from "next/font/google";
@@ -98,23 +17,6 @@ import Footer from "@/components/Footer";
 import { DayPickerProvider } from "react-day-picker";
 import PublishCourse from "@/components/publishcourse";
 import { ConnectButton } from "@rainbow-me/rainbowkit";
-// import '@rainbow-me/rainbowkit/styles.css';
-// import {
-//   getDefaultConfig,
-//   RainbowKitProvider,
-// } from '@rainbow-me/rainbowkit';
-// import { WagmiProvider } from 'wagmi';
-// import {
-//   mainnet,
-//   polygon,
-//   optimism,
-//   arbitrum,
-//   base,
-// } from 'wagmi/chains';
-// import {
-//   QueryClientProvider,
-//   QueryClient,
-// } from "@tanstack/react-query";
 
 
 const inter = Inter({ subsets: ["latin"] });
@@ -160,24 +62,5 @@ export default function RootLayout({
         </WagmiConfigProvider>
     </html>
     );
-  // return (
-  //   <html lang="en">
-    //  <WagmiProvider config={config}>
-    //   <QueryClientProvider client={queryClient}>
-    //     <RainbowKitProvider> 
-        // <WagmiConfigProvider>
-        // <Providers>
-        //     <Navbar />
-        //     {/* <body className={cn(poppins.className , fontHeading.variable)}>{children}</body> */}
-        //     {/* <body>{children}</body> */}
-        //     <PublishCourse/>
-        //   <Toaster />
-        //   <Footer/>
-        // </Providers>
-        // </WagmiConfigProvider>
-        {/* </RainbowKitProvider>
-      </QueryClientProvider>
-    </WagmiProvider> */}
-  //   </html>
-  // );
+
 }
